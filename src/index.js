5de422bcf718bc89de0f25fbd005acca530fd3e0
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-12-23 15:23:12 +0000

    Refactor and lint index.js to make App a class-based component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = `AIzaSyB04tePxGTIlgGGkt7dUy_3dozKjfHxoG8`;
+const API_KEY = 'AIzaSyB04tePxGTIlgGGkt7dUy_3dozKjfHxoG8';
 
 // Create a new component. This component should produce some HTML.
-const App = () => (
-  <div>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+      <div>
+        <SearchBar />
+      </div>
+		);
+	}
+}
 
 // Take this component's generated HTML and add it to the DOM.
 ReactDOM.render(<App />, document.querySelector('.container'));
