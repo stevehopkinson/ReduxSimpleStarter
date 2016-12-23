c2b249bea675128d56dbe40907892ab0b5535e15
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-12-23 13:35:30 +0000

    Update arrow function syntax in search bar event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-        <input onChange={ (event) => this.setState({ term: event.target.value }) } />
+        <input onChange={ event => this.setState({ term: event.target.value }) } />
       </div>
     );
   }
