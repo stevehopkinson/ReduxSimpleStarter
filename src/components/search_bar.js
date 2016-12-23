03ba41f75d000f53e6f300641686e7baf298ab91
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-12-23 15:01:57 +0000

    Lint search_bar.js

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = { term: '' };
-  }
+		this.state = { term: '' };
+	}
 
-  render() {
-    return (
+	render() {
+		return (
       <div>
-        <input onChange={ event => this.setState({ term: event.target.value }) } />
+        <input
+          value={this.state.term}
+          onChange={ event => this.setState({ term: event.target.value }) }
+        />
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default SearchBar;
