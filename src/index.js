5b304c7738480f20240af6b64a48e81413c0d934
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-12-24 10:37:21 +0000

    Add onSearchTermChange handler to search_bar

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ class App extends Component {
 
 		this.state = { videos: [], selectedVideo: null };
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term}, videos => {
 			this.setState({
 				videos: videos,
 				selectedVideo: videos[0]
@@ -25,11 +29,11 @@ class App extends Component {
 	render() {
 		return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
 				<VideoDetail video={this.state.selectedVideo}/>
         <VideoList
 					videos={this.state.videos}
-					onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) } 
+					onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
 				/>
       </div>
 		);
